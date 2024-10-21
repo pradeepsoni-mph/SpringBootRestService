58deab26118d491a3a56f05109a68ed91359fac1
Pradeepsoni-mph <pk.study40@gmail.com>
2024-10-21 22:51:08 +0530

    init

---
+++ src/test/java/com/ssc/SpringBootRestService/SpringBootRestServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssc.SpringBootRestService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRestServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
